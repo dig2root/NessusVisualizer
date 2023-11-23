ac94c389200fdd15fa71157a999cccd47eb74aef
Clément Rault <clem.rault@outlook.com>
2023-11-23 10:50:14 -0500

    Update base graph

--- main.py
+++ main.py
@@ -6,14 +6,23 @@ import numpy as np
 from src.view.View import View
 from src.controller.Controller import Controller
 
-# Create a new View instance
+# Initialize the app
 view = View.get_instance()
-# Create a new Controller instance
 controller = Controller.get_instance()
+
+# Callback for uploading files
+@view.callback(
+    dash.dependencies.Output("nessus-dropdown", "options"),
+    dash.dependencies.Input('nessus-uploader', 'isCompleted'))
+def update_dropdown(isCompleted):
+    subfolders = os.listdir(view.upload.get_upload_folder())
+    file_names = []
+    for subfolder in subfolders:
+        file_names.extend(os.listdir(os.path.join(view.upload.get_upload_folder(), subfolder)))
+    return file_names
 
 # Callback for dropdown value
 @view.callback(
-    dash.dependencies.Output("nessus-dropdown", "value"),
     dash.dependencies.Output("graph", "figure"),
     dash.dependencies.Input('nessus-dropdown', 'value'))
 def display_graph(value):
